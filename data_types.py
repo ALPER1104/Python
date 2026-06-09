9696d8f6ef2009b9d48b017b2486e6fca60eec30
M. Alper Yılmaz <m.alper4011@gmail.com>
2026-06-09 18:31:32 +0300

    data types exercise

---
+++ data_types.py
@@ -0,0 +1,28 @@
+# Değişken alıştırması
+ondalikli_sayi=10.934
+print(type(ondalikli_sayi))
+
+sayi=5
+print(type(sayi))
+
+green = True
+print(green)
+print(type(green))
+green = 1
+print(type(green))
+
+ad = input ("Adınızı giriniz: ")
+soyad = input ("Soyadınınz girininiz: ")
+isim = ad+ " " + soyad
+print(isim)
+print(type(isim))
+karakter_sayisi = len(isim)
+print("İsminizin toplam karakter sayısı:",karakter_sayisi)
+if karakter_sayisi<12:
+    print("isminizin tooplam karakter sayısı gerekli şartı sağlamamaktadır")
+
+else:
+    print("isminizin toplam karakter sayisi gerkli şartı sağlamaktadır diğer bilgilerinizi girmeye devam edebilirsiniz")
+
+
+
