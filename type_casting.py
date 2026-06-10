b85d803349446980a394b20f66d9813f9db51167
M. Alper Yılmaz <m.alper4011@gmail.com>
2026-06-10 23:37:14 +0300

    type casting exercise

---
+++ type_casting.py
@@ -0,0 +1,23 @@
+#Değişken dönüşümü alıştırması
+
+a= 22
+b= "33"
+print("toplam:", a + int("33") )
+
+yas = input("Lütfen yaşınızı giriniz:")
+
+karakter_sayisi = input("Lütfen adınızın kaç harften oluştuğunu giriniz: ")
+
+print("toplam:", int(yas) + int(karakter_sayisi))
+
+x = 23
+y = 13
+
+print( str(x) + str(y))
+
+z = 1.04
+t = "12.98"
+k = z + float("12.98")
+print(k)
+
+
