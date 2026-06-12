e0c1614b49f6cae0ac00fe28bf641b437fdcd2a9
M. Alper Yılmaz <m.alper4011@gmail.com>
2026-06-12 19:31:34 +0300

    arithmetic operations

---
+++ arithmetic_operation.py
@@ -0,0 +1,30 @@
+# Artimetik işlemler alıştırması
+
+x = 10
+y = 12.4
+z = x**y
+t = y//z
+k = y%x
+print(f"z: {z}  t:{t} k: {k}")
+
+# Yamuğun alanını hesaplayan program yazınız. Sonucu tam sayı olacak şekilde hesaplama yapınız
+
+a = 15 # alt taban uzunluğu
+b = 10 # üst taban
+h = 5  # yükseklik
+print("Yamuğun alanı:", ((a+b)*h)//2)
+
+# Beşgenin alanını hesaplayan bir program yazın. Değerleri kullanıcıdan alın
+
+a = float(input("Beşgenin kenar uzunluğunu giriniz:"))
+h = float(input("beşgenin apotem uzunluğunu giriniz:"))
+
+print ("Beşgenin alanı:", (5*a*h)/2 )
+
+
+
+
+
+
+
+
